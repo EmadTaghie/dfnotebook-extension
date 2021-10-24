397f5f25b0ea383ad271c60d6625f5c59e4cb74b
Emad <emad.taghiye@gmail.com>
2021-10-24 19:54:05 +0330

    dfnotebook/widgetfactory patched

--- dfnotebook/src/widgetfactory.ts
+++ dfnotebook/src/widgetfactory.ts
@@ -1,24 +1,18 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { IEditorMimeTypeService } from '@jupyterlab/codeeditor';
-
-import { ABCWidgetFactory, DocumentRegistry } from '@jupyterlab/docregistry';
-
-import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
-
-import { ToolbarItems } from './default-toolbar';
-
-import { INotebookModel } from './model';
-
-import { NotebookPanel } from './panel';
-
-import { StaticNotebook } from './widget';
-
 import {
   ISessionContextDialogs,
   sessionContextDialogs
 } from '@jupyterlab/apputils';
+import { IEditorMimeTypeService } from '@jupyterlab/codeeditor';
+import { ABCWidgetFactory, DocumentRegistry } from '@jupyterlab/docregistry';
+import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+import { ITranslator } from '@jupyterlab/translation';
+import { ToolbarItems } from './default-toolbar';
+import { INotebookModel } from './model';
+import { NotebookPanel } from './panel';
+import { StaticNotebook } from './widget';
 
 /**
  * A widget factory for notebook panels.
@@ -90,7 +84,7 @@ export class NotebookWidgetFactory extends ABCWidgetFactory<
     context: DocumentRegistry.IContext<INotebookModel>,
     source?: NotebookPanel
   ): NotebookPanel {
-    let nbOptions = {
+    const nbOptions = {
       rendermime: source
         ? source.content.rendermime
         : this.rendermime.clone({ resolver: context.urlResolver }),
@@ -101,7 +95,7 @@ export class NotebookWidgetFactory extends ABCWidgetFactory<
         ? source.content.notebookConfig
         : this._notebookConfig
     };
-    let content = this.contentFactory.createNotebook(nbOptions);
+    const content = this.contentFactory.createNotebook(nbOptions);
 
     return new NotebookPanel({ context, content });
   }
@@ -112,7 +106,11 @@ export class NotebookWidgetFactory extends ABCWidgetFactory<
   protected defaultToolbarFactory(
     widget: NotebookPanel
   ): DocumentRegistry.IToolbarItem[] {
-    return ToolbarItems.getDefaultItems(widget, this._sessionDialogs);
+    return ToolbarItems.getDefaultItems(
+      widget,
+      this._sessionDialogs,
+      this.translator
+    );
   }
 
   private _editorConfig: StaticNotebook.IEditorConfig;
@@ -158,6 +156,11 @@ export namespace NotebookWidgetFactory {
      * The session context dialogs.
      */
     sessionDialogs?: ISessionContextDialogs;
+
+    /**
+     * The application language translator.
+     */
+    translator?: ITranslator;
   }
 
   /**
